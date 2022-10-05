55e3ae665d6620fe7c7e0b3dc1bf799f17fe0504
dutaramadana <gungdeduta@gmail.com>
2022-10-05 16:29:01 +0800

    first commit

---
+++ src/main/java/com/program/main/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.program.main.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLogin(){
+        return "login-page";
+    }
+
+
+}
